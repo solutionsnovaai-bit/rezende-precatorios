3a990982566bb09e5c2d5da6dcda88a445c3f7d5
solutionsnovaai-bit <solutions.nova.ai@gmail.com>
2026-09-20 13:04:47 -0300

    Delete src directory

--- src/assets.ts
+++
@@ -1,6 +0,0 @@
-export function imageSrcSet(name: string) {
-  return `/images/${name}-640.webp 640w, /images/${name}-960.webp 960w, /images/${name}.webp 1448w`
-}
-
-export const gallerySizes = '(max-width: 768px) 100vw, (max-width: 1200px) calc(91.1vw - 320px), calc(91.1vw - 350px)'
-export const aboutSizes = '(max-width: 768px) calc(100vw - 61px), (max-width: 1400px) 42vw, 570px'
